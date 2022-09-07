ab8e78f34a8334ee2005cf99e52ac10095614107
RuchaRK <49073201+RuchaRK@users.noreply.github.com>
2022-09-07 19:11:08 +0530

    feat:Taking Input & identifying emoji

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import React, { useState } from "react";
 import "./styles.css";
 
+var emojiDictionery = {
+  "🚣": "Person Rowing Boat",
+  "🏕️": "Camping",
+  "🏖️": "Beach",
+  "🏞️": "National Park",
+  "🗻": "Mount Fuji",
+  "🏔️": "Snow-Capped Mountain",
+  "🗼": "Tokyo Tower",
+  "🎠": "Carousel Horse",
+  "🎑": "Moon Viewing Ceremony"
+};
+var travelSymbol = Object.keys(emojiDictionery);
+
 export default function App() {
+  var [meaning, setMeaning] = useState("");
+
+  function changeHandler(event) {
+    var userInput = event.target.value;
+    meaning = emojiDictionery[userInput];
+
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hey are u intrested in Flags!</h1>
+      <input onChange={changeHandler}></input>
+      <h2>{meaning}</h2>
+      <h3>Please select a emoji from the below shown emojis</h3>
+      <p>{travelSymbol}</p>
     </div>
   );
 }
